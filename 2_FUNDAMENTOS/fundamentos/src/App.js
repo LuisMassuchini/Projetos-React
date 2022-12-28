12eab44bca48986743b1025ad4729dbdbe5b1a32
Luís Massuchini <luis.massuchini@gmail.com>
2022-12-28 16:26:40 -0300

    feat: finalizando modulo 2

---
+++ 2_FUNDAMENTOS/fundamentos/src/App.js
@@ -0,0 +1,25 @@
+//components
+import FirstComponent from './components/FirstComponent';
+import TemplateExpressions from './components/TemplateExpressions';
+import MyComponent from './components/MyComponent';
+import Events from './components/Events';
+
+//styles/css
+import './App.css';
+import Challenge from './components/Challenge';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Fundamentos React</h1>
+      <FirstComponent/>
+      <TemplateExpressions/>
+      <MyComponent/>
+      <Events />
+      <Challenge/>     
+    </div>
+  );
+}
+
+export default App;
