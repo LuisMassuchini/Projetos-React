775f9a230558091b07c91cc6fc85d039fb9c493b
Luís Massuchini <luis.massuchini@gmail.com>
2023-01-04 14:53:48 -0300

    Aula 103 - Search Params

--- 8_REACT_ROUTER/react-router/src/App.js
+++ 8_REACT_ROUTER/react-router/src/App.js
@@ -9,9 +9,11 @@ import About from "./pages/About";
 import Product from "./pages/Product";
 import Info from "./pages/Info";
 import NotFound from "./pages/NotFound";
+import Search from "./pages/Search";
 
 // components
 import Navbar from "./components/Navbar";
+import SearchForm from "./components/SearchForm";
 
 function App() {
   return (
@@ -19,7 +21,9 @@ function App() {
       <h1>React Router</h1>
       <BrowserRouter>
         {/*2 - link com react router*/}
-        <Navbar></Navbar>
+        <Navbar/>
+        {/* 9 - search */}
+        <SearchForm/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
@@ -27,6 +31,8 @@ function App() {
           <Route path="/products/:id/info" element={<Info />} />
           {/*4 - routa dinamica*/}
           <Route path="/products/:id" element={<Product />} />
+          {/* 9 - search */}
+          <Route path="/search" element={<Search />}/>
           {/*7 - no match route*/}
           <Route path="*" element={<NotFound />}/>
         </Routes>
