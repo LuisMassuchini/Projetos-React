57c4b35495a424e755adb34fb1bf2ef43a2e38a3
Luís Massuchini <luis.massuchini@gmail.com>
2023-01-04 13:45:01 -0300

    Aula 99 carregamento dinamico de dados

--- 8_REACT_ROUTER/react-router/src/App.js
+++ 8_REACT_ROUTER/react-router/src/App.js
@@ -2,16 +2,27 @@ import "./App.css";
 
 //1 - config react router
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// pages
 import Home from "./pages/Home";
+import About from "./pages/About";
+import Product from "./pages/Product";
+
+// components
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
       <h1>React Router</h1>
       <BrowserRouter>
+        {/*2 - link com react router*/}
+        <Navbar></Navbar>
         <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/about" element={<About />}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          {/*4 - routa dinamica*/}
+          <Route path="/products/:id" element={<Product />} />
         </Routes>
       </BrowserRouter>
     </div>
