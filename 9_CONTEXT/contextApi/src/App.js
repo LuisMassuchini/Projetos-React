a0be8d6ece3de80f121bcdd542aaf66bcadca85b
Luís Massuchini <luis.massuchini@gmail.com>
2023-01-04 16:19:20 -0300

    aula 109 - Criando Provider

--- 9_CONTEXT/contextApi/src/App.js
+++ 9_CONTEXT/contextApi/src/App.js
@@ -1,10 +1,26 @@
-
 import './App.css';
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+
+// pages
+import Home from './pages/Home';
+import About from './pages/About';
+import Products from './pages/Products';
+
+//components
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <h1>Context</h1>
+      <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route to="/" element={<Home/>}/>
+        <Route to="/about" element={<About/>}/>
+        <Route to="/products" element={<Products />}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
