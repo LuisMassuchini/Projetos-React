7723412b25257a5a5762afa58f00d43aa6bf57a0
Luís Massuchini <luis.massuchini@gmail.com>
2023-01-10 22:41:41 -0300

    iniciando modulo10 - MiniBlog

---
+++ 10_MINIBLOG/miniblog/firebase/config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC1WJaBKda6xT83zsJ3UXzgqezb79cUZgo",
+  authDomain: "miniblog-e696d.firebaseapp.com",
+  projectId: "miniblog-e696d",
+  storageBucket: "miniblog-e696d.appspot.com",
+  messagingSenderId: "807347706013",
+  appId: "1:807347706013:web:446718ae6356e67a8b1c74",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+
+export { db };
