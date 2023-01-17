5fd36682f9952011a99fc7528f55ffdce8ee9557
Luís Massuchini <luis.massuchini@gmail.com>
2023-01-17 13:20:52 -0300

    Aula 135 - Estrutura da página de criar post

--- 10_MINIBLOG/miniblog/src/hooks/useAuthentication.js
+++ 10_MINIBLOG/miniblog/src/hooks/useAuthentication.js
@@ -14,8 +14,7 @@ export const useAuthentication = () => {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(null);
 
-  //cleanup
-  //deal with memory leak
+  // deal with memory leak
   const [cancelled, setCancelled] = useState(false);
 
   const auth = getAuth();
@@ -30,7 +29,6 @@ export const useAuthentication = () => {
     checkIfIsCancelled();
 
     setLoading(true);
-    setError(null);
 
     try {
       const { user } = await createUserWithEmailAndPassword(
@@ -43,8 +41,6 @@ export const useAuthentication = () => {
         displayName: data.displayName,
       });
 
-      setLoading(false);
-
       return user;
     } catch (error) {
       console.log(error.message);
@@ -53,16 +49,56 @@ export const useAuthentication = () => {
       let systemErrorMessage;
 
       if (error.message.includes("Password")) {
-        systemErrorMessage = "A senha precisa conter pelo menos 6 caracteres";
+        systemErrorMessage = "A senha precisa conter pelo menos 6 caracteres.";
       } else if (error.message.includes("email-already")) {
-        systemErrorMessage = "E-mail já cadastrado";
+        systemErrorMessage = "E-mail já cadastrado.";
       } else {
-        systemErrorMessage = "Ocorreu um erro, por favor tente mais tarde";
+        systemErrorMessage = "Ocorreu um erro, por favor tenta mais tarde.";
       }
 
-      setLoading(false);
       setError(systemErrorMessage);
     }
+
+    setLoading(false);
+  };
+
+  const logout = () => {
+    checkIfIsCancelled();
+
+    signOut(auth);
+  };
+
+  const login = async (data) => {
+    checkIfIsCancelled();
+
+    setLoading(true);
+    setError(false);
+
+    try {
+      await signInWithEmailAndPassword(auth, data.email, data.password);
+    } catch (error) {
+      console.log(error.message);
+      console.log(typeof error.message);
+      console.log(error.message.includes("user-not"));
+
+      let systemErrorMessage;
+
+      if (error.message.includes("user-not-found")) {
+        systemErrorMessage = "Usuário não encontrado.";
+      } else if (error.message.includes("wrong-password")) {
+        systemErrorMessage = "Senha incorreta.";
+      } else {
+        systemErrorMessage = "Ocorreu um erro, por favor tenta mais tarde.";
+      }
+
+      console.log(systemErrorMessage);
+
+      setError(systemErrorMessage);
+    }
+
+    console.log(error);
+
+    setLoading(false);
   };
 
   useEffect(() => {
@@ -73,6 +109,8 @@ export const useAuthentication = () => {
     auth,
     createUser,
     error,
+    logout,
+    login,
     loading,
   };
 };
--- 10_MINIBLOG/miniblog/src/pages/Register/Register.js
+++ 10_MINIBLOG/miniblog/src/pages/Register/Register.js
@@ -88,6 +88,7 @@ const Register = () => {
             onChange={(e) => setConfirmPassword(e.target.value)}
           />
         </label>
+        <button className="btn">Cadastrar</button>
         {!loading && <button className="btn">Cadastrar</button>}
         {loading && <button className="btn" disabled>Aguarde...</button>}
         {error && <p className="error">{error}</p>}
