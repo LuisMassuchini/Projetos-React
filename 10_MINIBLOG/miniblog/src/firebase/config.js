53ccfa7b1fae9c621a74fe3110252451d02ed9c3
Luís Massuchini <luis.massuchini@gmail.com>
2023-01-16 23:28:36 -0300

    Aula 131 - Bloqueando paginas que precisam de autenticacao

--- 10_MINIBLOG/miniblog/src/firebase/config.js
+++ 10_MINIBLOG/miniblog/src/firebase/config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firebase";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyC1WJaBKda6xT83zsJ3UXzgqezb79cUZgo",
